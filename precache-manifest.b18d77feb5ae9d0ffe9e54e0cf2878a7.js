7ed638144ba0d2adc947a58c1bc723bfbc11af8f
Alvin Fang <alfang@microsoft.com>
2020-07-28 14:21:00 -0700

    Updates

---
+++ precache-manifest.b18d77feb5ae9d0ffe9e54e0cf2878a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba67cdaf938aaaa0eb75",
+    "url": "/petSortingHatNew/static/css/main.fd83850b.chunk.css"
+  },
+  {
+    "revision": "ba67cdaf938aaaa0eb75",
+    "url": "/petSortingHatNew/static/js/main.ba67cdaf.chunk.js"
+  },
+  {
+    "revision": "fa465ae9d376806d1a3e",
+    "url": "/petSortingHatNew/static/js/1.fa465ae9.chunk.js"
+  },
+  {
+    "revision": "667f4b7c06f5d07055de",
+    "url": "/petSortingHatNew/static/js/runtime~main.667f4b7c.js"
+  },
+  {
+    "revision": "8928fff498731ab81428bf0e9ecaa57a",
+    "url": "/petSortingHatNew/static/media/hammy.8928fff4.png"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/petSortingHatNew/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "c6d9e93cc0ce0859bbd24b3405bca09f",
+    "url": "/petSortingHatNew/index.html"
+  }
+];
